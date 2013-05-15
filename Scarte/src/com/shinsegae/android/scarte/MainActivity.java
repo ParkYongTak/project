7825d486ebc633e6036a104ccb9c0d04556d5dbd
ParkYongTak <pwiner85@gmail.com>
2013-05-15 16:44:05 +0900

    layout

--- Scarte/src/com/shinsegae/android/scarte/MainActivity.java
+++ Scarte/src/com/shinsegae/android/scarte/MainActivity.java
@@ -1,15 +1,28 @@
 package com.shinsegae.android.scarte;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.widget.DigitalClock;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
+
+	private static final String URL = "http://www.sinc.co.kr/Upload/BuildCase_20120810083952_1.JPG";
+	//private static final String URL = "http://theeye.pe.kr/attach/1/1149348246.png";
+	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		DigitalClock dc = (DigitalClock) findViewById(R.id.clock);
+		// what can i do with DigitalClock also? for display only
+
+		ImageView imageView = (ImageView) findViewById(R.id.image_view);
+		ImageDownloader.download(URL, imageView);
+
 	}
 
 	@Override
