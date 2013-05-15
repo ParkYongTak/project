9ecb5423f7dc4be4286fffa4522d2ac6211a8312
gbkim <gbkim@sccl.co.kr>
2013-05-15 16:59:13 +0900

    한글 레이아웃

--- Scarte/src/com/shinsegae/android/scarte/RankingActivity.java
+++ Scarte/src/com/shinsegae/android/scarte/RankingActivity.java
@@ -8,5 +8,6 @@ public class RankingActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_ranking);
 	}
 }
--- Scarte/src/com/shinsegae/android/scarte/Settings.java
+++ Scarte/src/com/shinsegae/android/scarte/Settings.java
@@ -8,5 +8,6 @@ public class Settings extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_settings);
 	}
 }
